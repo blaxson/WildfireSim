59937b35b085625be52c51c4d8545190e51aead0
Bryce Laxson <Bryce.Laxson@wdc.com>
2022-01-17 15:30:29 -0800

    get weather data given coordinates of DEM from CLI; retrieved weather data includes temp, wind speed and direction, cloud cover, chance of percipitation, and percipitation intensity

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 import elevation
+import weather
 import os, sys
 import matplotlib 
 import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    if len(sys.argv) < 2:
-        print("usage: python3 main.py DEM.tif")
+    if len(sys.argv) < 4:
+        print("usage: python3 main.py DEM.tif latitude longitude")
         sys.exit(1)
     
+    ''' elevation data '''
     filename = sys.argv[1]
     try:
-        data = elevation.getDataFromFile(filename) # get elevation data from DEM
+        elevation_data = elevation.getDataFromFile(filename) # get elevation data from DEM
     except FileNotFoundError:
         print(f"{filename}: file not found")
         sys.exit(1)
     except elevation.FileNotSupportedError:
         print(f"{filename}: file not supported\n\tWildfireSim only supports .tif files")
         sys.exit(1)
-    if data is None:
+    if elevation_data is None:
         print(f"{filename}: could not open image file")
         sys.exit(1)
+
+    ''' weather data '''
+    apikey = 'H2TfW0sqrQG96bRwsszmg2Hb6gEh61As'
+    # latitude, longitude = 39.382558, 123.657235
+    latitude, longitude = sys.argv[2], sys.argv[3]
+    try:
+        weather_data = weather.getWeatherData(apikey, latitude, longitude)
+    except weather.WeatherAPIError as e:
+        print(e.message)
+        sys.exit(1)
     
+    print(weather_data)
+
     # TODO: temporary plotting of elevation data
     # remove once graphics are stable
-
-    print(len(data))
-    print(len(data[0]))
-    print(data)
+    
+    print(len(elevation_data))
+    print(len(elevation_data[0]))
+    print(elevation_data)
     fig = plt.figure(figsize = (12, 8))
     ax = fig.add_subplot(111)
-    plt.contour(data, cmap = "viridis", 
+    plt.contour(elevation_data, cmap = "viridis", 
                 levels = list(range(0, 4500, 100)))
     plt.title("Elevation Contours of Mt. Whitney")
     cbar = plt.colorbar()
---
+++ weather.py
@@ -0,0 +1,82 @@
+import requests
+import datetime
+import json
+import numpy as np
+
+class WeatherAPIError(Exception):
+    def __init__(self, message):
+        self.message = message
+    
+    def __str__(self):
+        return self.message
+
+class Weather:
+    def __init__(self, time, temperature, windSpeed, windDirection, windGust=None,
+                 cloudCover=None, precProb=None, precInt=None, precType=None):
+        self.time = time
+        self.temperature = temperature
+        self.windSpeed = windSpeed
+        self.windGust = windGust
+        self.windDirection = windDirection
+        self.cloudCover = cloudCover
+        self.precipitationProbability = precProb
+        ''' maybe not needed? '''
+        self.precipitationIntensity = precInt
+        self.precipitationType = precType 
+    
+    def __repr__(self):
+        return f"time: {self.time},\t temperature: {self.temperature},\t windSpeed: {self.windSpeed},\t " + \
+               f"windGust: {self.windGust},\t windDirection: {self.windDirection},\t cloudCover: {self.cloudCover},\t " + \
+               f"precipitationProbability: {self.precipitationProbability}"
+
+    def __str__(self):
+        return f"time: {self.time},\t temperature: {self.temperature},\t windSpeed: {self.windSpeed},\t " + \
+               f"windGust: {self.windGust},\t windDirection: {self.windDirection},\t cloudCover: {self.cloudCover},\t " + \
+               f"precipitationProbability: {self.precipitationProbability}"
+
+''' gets the weather data of the given latitude and longitude coordinates, 
+    returns np array of weather objects '''
+def getWeatherData(apikey, latitude, longitude):
+    # latitude ranges b/w -90 and 90, longitude b/w -180 and 180
+    if latitude < -90 or latitude > 90 or longitude < -180 or longitude > 180:
+        raise WeatherAPIError(f"latitude must be between -90 and 90 inclusive,\n" + \
+                              f"longitude must be between -180 and 180 inclusive;" + \
+                              f"given (lat, lon): {latitude}, {longitude}\n")
+
+    weatherURL = "https://api.tomorrow.io/v4/timelines"
+
+    querystring = {
+    "location":f"{latitude}, {longitude}",
+    "fields":["temperature", "windSpeed", "windGust", "windDirection", "cloudCover", 
+            "precipitationType", "precipitationProbability", "precipitationIntensity"],
+    "units":"imperial",
+    "timesteps":"1h",
+    "apikey":apikey
+    }
+
+    # make api request
+    r = requests.request("GET", weatherURL, params=querystring)
+    if r.status_code >= 400:
+        raise WeatherAPIError(r.text)
+    
+    # convert response to json and get weather entries
+    r_json = r.json()
+    entries = r_json['data']['timelines'][0]['intervals']
+
+    weather_points = []
+    for entry in entries: # extract weather data from each entry
+        time = entry['startTime']
+        values = entry['values']
+        weather = Weather(time=time, temperature=values['temperature'], windSpeed=values['windSpeed'], 
+                          windDirection=values['windDirection'], windGust=values['windGust'],
+                          cloudCover=values['cloudCover'], precProb=values['precipitationProbability'],
+                          precInt=values['precipitationIntensity'], precType=values['precipitationType'])
+        
+        weather_points.append(weather)
+
+    return np.asarray(weather_points)
+    '''
+    r.status_code
+    r.headers
+    r.url
+    '''
